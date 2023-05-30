b5232581f02a3344d4ee4e955a3fc0d65e90e942
yuliya <liana-yuliana@list.ru>
2023-05-30 18:37:20 +0300

    Added Task023

---
+++ task023/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+int Prompt (string msg)
+{
+    Console.Write (msg);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+void CubeNumber (int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        double cubeNum = Math.Pow(i, 3);
+        Console.Write($"{cubeNum} ");
+    }
+}
+int num = Prompt("Введите число: ");
+CubeNumber(num);
